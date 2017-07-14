1e6292d9efd710d5c3d0f9a802c03ca634c41ced
Ninjseal <b.marius10@yahoo.com>
2017-07-14 20:40:41 +0300

    Box2D Demo
    Just a demo...

--- core/src/com/gdxcollab/game/GameMain.java
+++ core/src/com/gdxcollab/game/GameMain.java
@@ -3,31 +3,89 @@ package com.gdxcollab.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class GameMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	private OrthographicCamera camera;
+
+	private World world;
+	private Body player, platform;
+	private Box2DDebugRenderer b2dr;
 	
+	public static final float PPM = 32f;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		float h = Gdx.graphics.getWidth();
+		float w = Gdx.graphics.getHeight();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, w / 2, h / 2);
+		b2dr = new Box2DDebugRenderer();
+		world = new World(new Vector2(0, -9.8f), false);
+		player = createPlayer();
+		platform = createPlatform();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+
+		update(Gdx.graphics.getDeltaTime());
+
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		b2dr.render(world, camera.combined.cpy().scl(PPM));
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		camera.setToOrtho(false, width, height);
+	}
+
+	@Override
+	public void dispose() {
+		world.dispose();
+		b2dr.dispose();
+	}
+
+	public void update(float delta) {
+		world.step(1 / 60f, 6, 2);
+	}
+
+	public Body createPlayer() {
+		Body playerBody;
+		BodyDef def = new BodyDef();
+		def.type = BodyDef.BodyType.DynamicBody;
+		def.position.set(200 / PPM, 300 / PPM);
+		def.fixedRotation = true;
+		playerBody = world.createBody(def);
+		Shape shape = new PolygonShape();
+		((PolygonShape) shape).setAsBox(25 / PPM, 25 / PPM);
+		playerBody.createFixture(shape, 1.0f);
+		shape.dispose();
+		return playerBody;
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public Body createPlatform() {
+		Body platformBody;
+		BodyDef bDef = new BodyDef();
+		bDef.type = BodyDef.BodyType.StaticBody;
+		bDef.position.set(170 / PPM, 50 / PPM);
+		platformBody = world.createBody(bDef);
+		Shape shape = new PolygonShape();
+		((PolygonShape)shape).setAsBox(70 / PPM, 50 / PPM);
+		platformBody.createFixture(shape, 1.0f);
+		shape.dispose();
+		return platformBody;
 	}
+	
 }
--- desktop/src/com/gdxcollab/game/desktop/DesktopLauncher.java
+++ desktop/src/com/gdxcollab/game/desktop/DesktopLauncher.java
@@ -1,5 +1,7 @@
 package com.gdxcollab.game.desktop;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gdxcollab.game.GameMain;
@@ -7,6 +9,17 @@ import com.gdxcollab.game.GameMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GameMain(), config);
+		config.title = "Box2D Test";
+		config.useGL30 = false;
+		config.width = 800;
+		config.height = 480;
+		Application app = new LwjglApplication(new GameMain(), config);
+		
+		Gdx.app = app;
+		
+		// Gdx.app.setLogLevel(Application.LOG_INFO);
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		// Gdx.app.setLogLevel(Application.LOG_ERROR);
+		// Gdx.app.setLogLevel(Application.LOG_NONE); 
 	}
 }
